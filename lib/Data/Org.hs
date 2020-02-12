62514348b30311bac2947f3371396b53f1cb3e88
Colin Woodbury <colin@fosskers.ca>
2020-02-12 13:00:13 -0800

    Basic markup parsing

--- lib/Data/Org.hs
+++ lib/Data/Org.hs
@@ -1,32 +1,54 @@
+{-# LANGUAGE DerivingStrategies #-}
+
 module Data.Org
   ( -- * Types
     Org(..)
   , Words(..)
+  , URL(..)
+  , Language(..)
     -- * Parser
   , org
   ) where
 
+import           Control.Applicative.Combinators.NonEmpty
+import           Control.Monad (void)
+import qualified Data.List.NonEmpty as NEL
+import           Data.Semigroup (sconcat)
 import           Data.Text (Text)
 import           Data.Void (Void)
-import           Text.Megaparsec
+import           Text.Megaparsec hiding (sepBy1, sepEndBy1, some, someTill)
+import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
 --------------------------------------------------------------------------------
 -- Types
 
 -- | An org-mode document tree.
-data Org = Heading Org
-         | Quote Text
-         | Example Text
-         | Code Text
-         | Paragraph [Words]
-         | Meta
+data Org
+  = Heading Int (NEL.NonEmpty Words)
+  | Quote Text
+  | Example Text
+  | Code (Maybe Language) Text
+  | Paragraph (NEL.NonEmpty Words)
+  deriving stock (Eq, Show)
 
-data Words = Bold Text
-           | Italic Text
-           | Highlight Text
-           | Strikethrough Text
-           | Plain Text
+data Words
+  = Bold Text
+  | Italic Text
+  | Highlight Text
+  | Underline Text
+  | Verbatim Text
+  | Strike Text
+  | Link URL (Maybe Text)
+  | Image URL
+  | Plain Text
+  deriving stock (Eq, Show)
+
+-- | The url portion of a link.
+newtype URL = URL Text deriving stock (Eq, Show)
+
+-- | The programming language some source code block was written in.
+newtype Language = Language Text deriving stock (Eq, Show)
 
 --------------------------------------------------------------------------------
 -- Parser
@@ -35,13 +57,15 @@ type Parser = Parsec Void Text
 
 -- TODO Handle whitespace.
 org :: Parser [Org]
-org = many block
-
-block :: Parser Org
-block = choice [ heading, quote, example, code, paragraph, meta ]
+-- org = many (choice [heading, quote, example, code, paragraph]) <* eof
+org = many (choice [try heading, paragraph]) <* eof
 
 heading :: Parser Org
-heading = undefined
+heading = L.lexeme space $ do
+  stars <- some $ single '*'
+  void $ single ' '
+  l <- line
+  pure $ Heading (NEL.length stars) l
 
 quote :: Parser Org
 quote = undefined
@@ -53,7 +77,38 @@ code :: Parser Org
 code = undefined
 
 paragraph :: Parser Org
-paragraph = undefined
+paragraph = L.lexeme space $ Paragraph . sconcat <$> sepEndBy1 line newline
 
-meta :: Parser Org
-meta = undefined
+line :: Parser (NEL.NonEmpty Words)
+line = some wordChunk
+
+wordChunk :: Parser Words
+wordChunk = choice
+  [ Bold      <$> between (single '*') (single '*') (takeWhile1P Nothing (/= '*'))
+  , Italic    <$> between (single '/') (single '/') (takeWhile1P Nothing (/= '/'))
+  , Highlight <$> between (single '~') (single '~') (takeWhile1P Nothing (/= '~'))
+  , Verbatim  <$> between (single '=') (single '=') (takeWhile1P Nothing (/= '='))
+  , Underline <$> between (single '_') (single '_') (takeWhile1P Nothing (/= '_'))
+  , Strike    <$> between (single '+') (single '+') (takeWhile1P Nothing (/= '+'))
+  , try image
+  , link
+  , Plain     <$> takeWhile1P Nothing (/= '\n')
+  ]
+
+image :: Parser Words
+image = between (single '[') (single ']') $
+  between (single '[') (single ']') $ do
+    path <- takeWhile1P Nothing (/= '.')
+    void $ single '.'
+    ext <- string "jpg" <|> string "jpeg" <|> string "png"
+    pure . Image . URL $ path <> "." <> ext
+
+-- | TODO Image support. For lucid conversion, one will be `a` and the other
+-- will be an `img` element.
+link :: Parser Words
+link = between (single '[') (single ']') $ Link
+  <$> between (single '[') (single ']') (URL <$> takeWhile1P Nothing (/= ']'))
+  <*> optional (between (single '[') (single ']') (takeWhile1P Nothing (/= ']')))
+
+-- meta :: Parser Org
+-- meta = undefined
--- test/Test.hs
+++ test/Test.hs
@@ -1,4 +1,42 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Main where
 
+import Data.Org
+import Test.Tasty
+import Test.Tasty.HUnit
+import Text.Megaparsec
+
+---
+
 main :: IO ()
-main = pure ()
+main = defaultMain suite
+
+suite :: TestTree
+suite = testGroup "Unit Tests"
+  [ testGroup "Basic Markup"
+    [ testCase "Header" $ parseMaybe org "* Header"
+      @?= Just [Heading 1 [Plain "Header"]]
+    , testCase "Headers" $ parseMaybe org "* Header\n** Subheader"
+      @?= Just [Heading 1 [Plain "Header"], Heading 2 [Plain "Subheader"]]
+    , testCase "Bold" $ parseMaybe org "*Bold*"
+      @?= Just [Paragraph [Bold "Bold"]]
+    , testCase "Italics" $ parseMaybe org "/Italic/"
+      @?= Just [Paragraph [Italic "Italic"]]
+    , testCase "Highlight" $ parseMaybe org "~Highlight~"
+      @?= Just [Paragraph [Highlight "Highlight"]]
+    , testCase "Verbatim" $ parseMaybe org "=Verbatim="
+      @?= Just [Paragraph [Verbatim "Verbatim"]]
+    , testCase "Underline" $ parseMaybe org "_Underline_"
+      @?= Just [Paragraph [Underline "Underline"]]
+    , testCase "Strike" $ parseMaybe org "+Strike+"
+      @?= Just [Paragraph [Strike "Strike"]]
+    , testCase "Link" $ parseMaybe org "[[https://www.fosskers.ca][Site]]"
+      @?= Just [Paragraph [Link (URL "https://www.fosskers.ca") (Just "Site")]]
+    , testCase "Link (no desc)" $ parseMaybe org "[[https://www.fosskers.ca]]"
+      @?= Just [Paragraph [Link (URL "https://www.fosskers.ca") Nothing]]
+    , testCase "Image" $ parseMaybe org "[[/path/to/img.jpeg]]"
+      @?= Just [Paragraph [Image (URL "/path/to/img.jpeg")]]
+    ]
+  , testGroup "Complex Files" []
+  ]
